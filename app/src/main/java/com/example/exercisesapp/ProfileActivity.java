4db3108584b7324860750617ee8596f41c7e87ef
Josh-Cox <joshcox2@hotmail.com>
2023-02-14 16:32:47 +0000

    Profile Side Menu

--- app/src/main/java/com/example/exercisesapp/ProfileActivity.java
+++ app/src/main/java/com/example/exercisesapp/ProfileActivity.java
@@ -2,16 +2,26 @@ package com.example.exercisesapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.balysv.materialmenu.MaterialMenuDrawable;
+import com.google.android.material.navigation.NavigationBarItemView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
 
 import org.parceler.Parcels;
 
@@ -36,8 +46,18 @@ public class ProfileActivity extends AppCompatActivity implements RVInterface {
         setContentView(R.layout.activity_profile);
 
         // top action bar
+        ImageView menuIcon = findViewById(R.id.menuOrAddIcon);
         tvActionBar = findViewById(R.id.pageTitle);
         tvActionBar.setText(R.string.profile_page);
+
+        // side menu
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.side_nav_view);
+        View header = navigationView.getHeaderView(0);
+        ImageView backFromMenuIcon = header.findViewById(R.id.backFromMenu);
+        TextView sideMenuText = header.findViewById(R.id.pageTitle);
+        sideMenuText.setText("Customize");
+
 
         // set attributes
         recyclerView = findViewById(R.id.rvSavedList);
@@ -53,6 +73,39 @@ public class ProfileActivity extends AppCompatActivity implements RVInterface {
         recyclerView.setLayoutManager(new LinearLayoutManager(ProfileActivity.this));
 
 
+        // top action bar listener
+        menuIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.END);
+            }
+        });
+
+        // side menu listeners
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int id = item.getItemId();
+                drawerLayout.closeDrawer(GravityCompat.END);
+
+                switch(id) {
+                    case R.id.item1:
+                        Toast.makeText(context, "Item 1 pressed", Toast.LENGTH_SHORT).show();
+                    case R.id.item2:
+                        Toast.makeText(context, "Item 2 pressed", Toast.LENGTH_SHORT).show();
+                    default:
+                        return true;
+                }
+            }
+        });
+
+        backFromMenuIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.closeDrawer(GravityCompat.END);
+            }
+        });
 
 
         // bottom nav bar
